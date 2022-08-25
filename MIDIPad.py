b1c34597154980e0aaf71848b7a77123995477d9
Ashley <ashleydylanrudd@gmail.com>
2022-08-25 13:31:27 +0100

    Reimplimentd the MIDI Ppoll routine

--- MIDIPad.py
+++ MIDIPad.py
@@ -1,13 +1,29 @@
 #ADBeta 
-#21 Aug 2022
-#Version 0.7
+#25 Aug 2022
+#Version 0.9
 
 import midi
+import time
 
+# Init #########################################################################
 #Open the midi device with the selected port
 midi.open(1);
 
-
 print("Polling the MIDI Device. Press Control-C to exit.")
-midi.poll();
+
+# Main program loop ############################################################
+try:
+	timer = time.time()
+	while True:
+		midi.poll();
+			
+		time.sleep(0.0005)
+except KeyboardInterrupt:
+	print('')
+
+# Cleanup ######################################################################	
+finally:
+	midi.close()
+
+
 
--- midi.py
+++ midi.py
@@ -1,22 +1,19 @@
-#ADBeta 
-#21 Aug 2022
-#Version 0.7
+#ADBeta
 
-import time
 import rtmidi
 
 #MIDI input/output devices, force ALSA API
 midiout = rtmidi.MidiOut(rtapi=rtmidi.API_LINUX_ALSA)
 midiin = rtmidi.MidiIn(rtapi=rtmidi.API_LINUX_ALSA)
 
-
 #Array of button states
 MIDIBtnState = [0] * 127
 
 #List all availible MIDI Ports/Devices
 def list():
-	print("list");
-	#TODO
+	ports = midiout.get_ports()
+	print("ports: ")
+	print(ports);
 
 #Open specific port, this is the input and output device
 def open(port):
@@ -30,39 +27,28 @@ def open(port):
 
 #Constantly poll the port for new messages
 def poll():
-	try:
-		timer = time.time()
-		while True:
-			#Get a MIDI message packet from buffer
-			msg = midiin.get_message()
-		
-			#If the packet has information
-			if msg:
-				
-				#Get the status byte and the 'pitch' byte
-				status = int(msg[0][0])
-				pitch = int(msg[0][1])
-				
-				#Set the button at -pitch- to 0 or 1 depending on status
-				if status == 144:
-					MIDIBtnState[pitch] = 1
-					
-					#Set the button light to on
-					btnLightCall(pitch, 5)
+	#Get a MIDI message packet from buffer
+	msg = midiin.get_message()
 			
-				if status == 128:
-					MIDIBtnState[pitch] = 0
+	#If the packet has information
+	if msg:
+			
+		#Get the status byte and the 'pitch' byte
+		status = int(msg[0][0])
+		pitch = int(msg[0][1])
+				
+		#Set the button at -pitch- to 0 or 1 depending on status
+		if status == 144:
+			MIDIBtnState[pitch] = 1
+			#Set the button light to on
+			btnLightCall(pitch, 5)
+			
+		if status == 128:
+			MIDIBtnState[pitch] = 0		
+			#Set the button light to off
+			btnLightCall(pitch, 0)
 					
-					#Set the button light to off
-					btnLightCall(pitch, 0)
 					
-			#Sleep for some amount of time. Laggy at 0.05 but less CPU
-			time.sleep(0.0005)
-	except KeyboardInterrupt:
-		print('')
-	finally:
-		close()
-
 #Manage the output lighting based on call 
 def btnLightCall(pitch: int, val: int):
 	midiout.send_message([144, pitch, val])
